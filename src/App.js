792dc1fa545c8a7dee4024aa7c16e6976cd05215
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-02-18 18:15:50 +0100

    new reducer for customers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash })
+  }
+
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash })
+  }
+
   return (
     <div className="App">
       <h1>Redux Course</h1>
-      
+      <h2>Cash: {cash}</h2>
+      <div className='controls'>
+        <button onClick={() => addCash(Number(prompt()))}>Add Cash</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get Cash</button>
+      </div>
     </div>
   );
 }
