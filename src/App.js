c4408ecc980cecf755c6565e7c2efc0de2e2e7a0
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-02-18 20:23:05 +0100

    Constants for types and actions added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux'
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
 
   const addCash = (cash) => {
     dispatch({ type: "ADD_CASH", payload: cash })
@@ -13,6 +15,18 @@ function App() {
     dispatch({ type: "GET_CASH", payload: cash })
   }
 
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
+
   return (
     <div className="App">
       <h1>Redux Course</h1>
@@ -20,7 +34,20 @@ function App() {
       <div className='controls'>
         <button onClick={() => addCash(Number(prompt()))}>Add Cash</button>
         <button onClick={() => getCash(Number(prompt()))}>Get Cash</button>
+        <button onClick={() => addCustomer(prompt())}>Add Customer</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get Customers</button>
       </div>
+      {customers.length > 0 ?
+        <div className='customers'>
+          {customers.map(customer =>
+            <div onClick={() => removeCustomer(customer)}>{customer.name}</div>
+          )}
+        </div>
+        :
+        <div>
+          <h2>No Customers</h2>
+        </div>
+      }
     </div>
   );
 }
